5f7847d77068c424646e916546af26e93b5d949f
Adi Kantawala <adikan@ucla.edu>
2018-10-25 20:28:53 -0700

    Add Stack class

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -1,1 +1,17 @@
-puts "gell"
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def push(el)
+    @stack.push(el)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack.last
+  end
+end
