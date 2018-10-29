e1874221254cf9947f079534c93a6280b76ac9f2
Adi Kantawala <adikan@ucla.edu>
2018-10-28 18:58:14 -0700

    Finish Simon

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -4,19 +4,32 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    take_turn
+    until self.game_over
+      take_turn
+    end
+    reset_game
+    game_over_message
   end
 
-  def take_turn
 
+
+  def take_turn
+    show_sequence
+    require_sequence
+
+    round_success_message
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +37,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
@@ -32,10 +45,12 @@ class Simon
   end
 
   def game_over_message
-
+    puts "Sorry u lost"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
