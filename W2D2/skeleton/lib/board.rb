ad6b0e27e77a2dec34466429558528655c486337
Adi Kantawala <adikan@ucla.edu>
2018-10-29 23:20:28 -0700

    Working make move

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -23,20 +23,23 @@ class Board
 
   def valid_move?(start_pos)
     raise 'Invalid starting cup' if start_pos > 12 || start_pos < 0
-    raise "Starting cup is empty" if self.cups[start_pos].empty?
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
     stones = @cups[start_pos]
     @cups[start_pos] = []
+
     cup_idx = start_pos
     until stones.empty?
       cup_idx += 1
+
       cup_idx = 0 if cup_idx > 13
+
       if cup_idx == 6
-        @cups[6] << stones.pop if @name1 == @current_player_name
+        @cups[6] << stones.pop if @name1 == current_player_name
       elsif cup_idx == 13
-        @cups[13] << stones.pop if @name2 == @current_player_name
+        @cups[13] << stones.pop if @name2 == current_player_name
       else
         @cups[cup_idx] << stones.pop
       end
